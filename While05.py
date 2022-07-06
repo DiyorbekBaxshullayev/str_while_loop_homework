aedb1078e2bc3a4992389cf6653392463c159679
Diyorbek <baxshullayevd4@gmail.com>
2022-07-06 09:54:15 +0500

    5

--- While05.py
+++ While05.py
@@ -6,4 +6,13 @@ def main(s):
     Returns:
         int: return answer
     """
-    return
+    a=0
+    i=0 
+    while i < len(s):
+        if str(s[i]).islower():
+            a+=1
+        i+=1
+    
+    return a
+
+print(main('CodeschoolUz'))
