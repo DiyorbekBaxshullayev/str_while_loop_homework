2cad4019a1d9aa920cf44b3aaa599324c974cef7
Diyorbek <baxshullayevd4@gmail.com>
2022-07-06 10:09:54 +0500

    6

--- While06.py
+++ While06.py
@@ -7,3 +7,19 @@ def main(s):
     Returns:
         int: return answer
     """
+    a=0
+    i=0
+    s = str(s).lower()
+    while i<len(s):
+        if str(s[i]).isalpha():
+            if str(s[i]) == "a" or str(s[i]) == "o" or str(s[i]) == "u" or str(s[i]) == "e" or str(s[i]) == "i":
+                a+=0
+
+            else:
+                a+=1
+
+        i+=1
+    
+    return a
+
+print(main('CodeschoolUz'))
